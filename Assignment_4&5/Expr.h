10262305e846de1afd258bc0cb456ec3e377531e
randonreddy <randon.reddy.2023@mumail.ie>
2024-12-01 15:04:54 +0000

    Started Assignment 4&5

---
+++ Assignment_4&5/Expr.h
@@ -0,0 +1,14 @@
+//
+// Created by Randon Reddy on 01/12/2024.
+//
+
+#ifndef CS264_EXPR_H
+#define CS264_EXPR_H
+#include <string>
+
+class Expr {
+    virtual std::string toStr() = 0; //defining pure virtual function toStr
+};
+
+
+#endif //CS264_EXPR_H
